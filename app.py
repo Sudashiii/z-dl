ab86b82dc94830aab35f4896fef07fe1cb4e80fc
Sascha <salorenz01@proton.me>
2025-10-29 20:05:05 +0100

    default port

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def update_credentials():
         return jsonify({"error": f"Error updating credentials: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=6126, debug=True)
